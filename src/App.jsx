4f7fd845e5e75bbef925f06808167194a7724abf
Ismay_Khanim <semsmilev@gmail.com>
2024-12-17 11:26:14 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Homepage from "./homepage/HomePage";
+import Login from "./login/Login"
+import Register from "./register/Register"
+import { BrowserRouter, Routes, Route } from "react-router";
+
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
